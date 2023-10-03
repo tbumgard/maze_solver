65d11c6c87c12eab18ca6ff870e43040f7c69851
tbumgard <tbumgard@gmail.com>
2023-10-02 21:43:11 -0400

    updated cell class to include draw_move a gray/red line drawn between to cells

--- main.py
+++ main.py
@@ -61,6 +61,18 @@ class Cell:
         self.__y2 = y2
         self.__win = win
 
+    def get_x1(self):
+        return self.__x1
+    
+    def get_x2(self):
+        return self.__x2
+    
+    def get_y1(self):
+        return self.__y1
+    
+    def get_y2(self):
+        return self.__y2
+    
     def draw(self):
         if self.has_left_wall:
             self.__win.draw_line(Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2)), "black")
@@ -74,6 +86,16 @@ class Cell:
         if self.has_bottom_wall:
             self.__win.draw_line(Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2)), "black")
 
+    def draw_move(self, to_cell, undo=False):
+        if undo:
+            color = "gray"
+        else:
+            color = "red"
+
+        self.__win.draw_line(Line(Point((self.__x1 + self.__x2) / 2,
+                                        (self.__y1 + self.__y2) / 2),
+                                        Point((to_cell.get_x1() + to_cell.get_x2()) / 2,
+                                              (to_cell.get_y1() + to_cell.get_y2()) / 2)), color)
 
 def main():
 
@@ -81,10 +103,15 @@ def main():
     point1 = Point(50, 50)
     point2 = Point(100, 100)
     line1 = Line(point1, point2)
-    win.draw_line(line1, "black")
+    ##win.draw_line(line1, "black")
 
-    new_cell = Cell(200, 400, 200, 400, win)
-    new_cell.draw()
+    cell1 = Cell(10, 20, 10, 20, win)
+    cell2 = Cell(20, 30, 10, 20, win)
+    
+    cell1.draw()
+    cell2.draw()
+    
+    cell1.draw_move(cell2)
 
     win.wait_for_close()
 
